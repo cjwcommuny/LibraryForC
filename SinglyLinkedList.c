3c9c0b18f15d8128423f691b020f7eecab2d8919
Chen Jiawei <cjwcommuny@outlook.com>
2017-03-29 21:04:49 +0800

    singly linked list

---
+++ SinglyLinkedList.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "genlib.h"
+#include "SinglyLinkedList.h"
+
+struct llist *list_new(void)
+{
+    struct llist *llist;
+
+    llist = GetBlock(sizeof(struct llist));
+    llist->size = 0;
+    llist->head = NULL;
+    return llist;
+}
+
+void list_add(struct llist *list, void *value)
+{
+    struct node *newnode;
+
+    newnode = GetBlock(sizeof(struct node));
+    newnode->value = value;
+    newnode->next = list->head;
+    list->head = newnode;
+    list->size++;
+}
+
+void list_append(struct llist *list, void *value)
+{
+    struct node *newnode, **travel;
+
+    newnode = GetBlock(sizeof(struct node));
+    newnode->value = value;
+    newnode->next = NULL;
+    travel = &(list->head);
+    while (*travel) {
+        travel = &((*travel)->next);
+    }
+    *travel = newnode;
+}
+
+int list_size(struct llist *list)
+{
+    return list->size;
+}
+
+void list_delete(struct llist *list)
+{
+    struct node *temp;
+
+    temp = list->head->next;
+    free(list->head);
+    list->head = temp;
+}
+
+void list_destory(struct llist *list)
+{
+    while (list->head) {
+        list_delete(list);
+    }
+    free(list);
+}
+
+
+
+
+
---
+++ SinglyLinkedList.h
@@ -0,0 +1,21 @@
+#ifndef _SinglyLinkedList_h
+#define _SinglyLinkedList_h
+
+struct node {
+    void *value;
+    struct node *next;
+};
+
+struct llist {
+    struct node *head;
+    int size;
+};
+
+struct llist *list_new(void);
+void list_append(struct llist *list, void *value);
+void list_append(struct llist *list, void *value);
+int list_size(struct llist *list);
+void list_delete(struct llist *list);
+void list_destory(struct llist *list);
+
+#endif
